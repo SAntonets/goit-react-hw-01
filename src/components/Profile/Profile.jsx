6e98450d49f368d3a9cd228746fb563d77b02da5
Serhii Antonets <antonets.serhii.ua@gmail.com>
2024-03-09 21:56:52 +0200

    Profile done

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -19,18 +19,18 @@ alt="User avatar"
  <p className={css.profileMainInfo }>{location}</p>
 </div>
 
-<ul>
-<li>
-<span>Followers</span>
-<span>{followers}</span>
+<ul className={css.profileStats}>
+<li className={css.profileStatsItem}>
+<span className={css.profileStatsTitle}>Followers</span>
+<span className={css.profileStatsValue}>{followers}</span>
 </li>
-<li>
-<span>Views</span>
-                  <span>{views}</span>
+<li className={css.profileStatsItem}>
+<span className={css.profileStatsTitle}>Views</span>
+                  <span className={css.profileStatsValue}>{views}</span>
 </li>
-<li>
-<span>Likes</span>
-<span>{likes}</span>
+<li className={css.profileStatsItem}>
+<span className={css.profileStatsTitle}>Likes</span>
+<span className={css.profileStatsValue}>{likes}</span>
 </li>
 </ul>
 </div>
